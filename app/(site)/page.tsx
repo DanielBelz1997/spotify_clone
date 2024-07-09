9a04537c6eddb9278f3969f006a6d2ae161ab7ee
BelzDaniel <BelzDaniel6@gmail.com>
2024-07-09 21:33:23 +0300

    feat: initlize db

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -10,9 +10,21 @@ export default function Home() {
             Welcome back
           </h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4"></div>
-          <ListItem />
+          <ListItem
+            image="/images/liked.jpg"
+            name="Liked Songs"
+            href="liked"
+          />
         </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">
+            Newest songs
+          </h1>
+        </div>
+        <div>List Of Songs!</div>
+      </div>
     </div>
   );
 }
